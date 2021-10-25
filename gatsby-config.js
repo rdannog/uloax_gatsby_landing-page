fd9899cfd37212d52ecc56290c1292a749bb1f8f
rhayssadandara <rhayssadandara98@gmail.com>
2021-10-25 19:58:34 -0300

    adding plugins and graphcms endpoint url

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,15 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "uloax",
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-react-helmet"],
+  plugins: ["gatsby-plugin-styled-components", 
+  "gatsby-plugin-react-helmet",
+  {
+    resolve: "gatsby-source-graphql",
+      options: {
+        typeName: "alldata",
+        fieldName: "alldata",
+        url: "https://api-us-east-1.graphcms.com/v2/ckv72s68x4vxt01zd5xpseznz/master",
+      },
+  }
+],
 };
