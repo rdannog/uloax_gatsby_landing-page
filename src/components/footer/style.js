1e0ac4ba5c8baac4a8bc62abce34dbc30c6de457
marxxbluecode <marxx171@gmail.com>
2021-10-27 19:48:29 -0300

    confirmar

--- src/components/footer/style.js
+++ src/components/footer/style.js
@@ -1,1 +1,26 @@
 import styled from "styled-components";
+
+export const FooterContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    width: 100%;
+    background-color: #1a1a1a;
+`
+
+export const Adress = styled.div`
+    display: flex;
+`
+
+export const Links = styled.div`
+    display: flex;
+`
+
+export const Follow = styled.div`
+    display: flex;
+`
+
+export const News = styled.div`
+    display: flex;
+`
