49642d71ea660b911daeda84f5a7ce03213e3500
marxxbluecode <marxx171@gmail.com>
2021-10-27 18:10:54 -0300

    confirmar

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,8 +2,16 @@ import React from 'react'
 import * as S from './style'
 export function Header() {
     return (
-        <div>
-            <p>oi</p>
-        </div>
+        <S.HeaderContainer>
+            <S.Nav>
+                <S.NavLogo/>
+                    <S.List>
+                        <S.Items>Home</S.Items>
+                        <S.Items>Taxi</S.Items>
+                        <S.Items>Booking</S.Items>
+                        <S.Items>Contact Us</S.Items>
+                    </S.List>
+            </S.Nav>
+        </S.HeaderContainer>
     )
 }
